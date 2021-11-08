e8bf7f7f02dd5e0e65a8aed91ac0048031e3e83d
Lizethsuk <julia.sucasacai@gmail.com>
2021-11-08 16:28:14 -0500

    first

---
+++ js/main.js
@@ -0,0 +1,342 @@
+/*
+
+	Template Name: Exhibit - Conference & Event HTML Template
+	Author: Themewinter
+	Author URI: https://themeforest.net/user/themewinter
+	Description: Exhibit - Conference & Event HTML Template
+	Version: 1.0
+   =====================
+   table of content 
+   ====================
+   1.   menu toogle
+   2.   event counter
+   3.   funfact
+   4.   isotope grid
+   5.   main slider
+   6.   speaker popup
+   7.   gallery
+   8.   video popup
+   9.   hero area image animation
+   10.  wow animated
+   11.  back to top
+  
+*/
+const body= document.body;
+const triggerMenu= document.querySelector(".page-header .trigger-menu");
+const nav= document.querySelector(".page-header .nav");
+const scrollUp= "scroll-up";
+const scrollDown= "scroll-down";
+const card1= document.querySelector(".card1");
+const card2= document.querySelector(".card2");
+const card3= document.querySelector(".card3");
+const card4= document.querySelector(".card4");
+const card5= document.querySelector(".card5");
+const card6= document.querySelector(".card6");
+
+let lastScroll = 0;
+
+
+
+card1.addEventListener("click", () => {
+   
+   card1.classList.toggle('is-flipped');
+
+ });
+ card2.addEventListener("click", () => {
+   card2.classList.toggle('is-flipped');
+
+ });
+ card3.addEventListener("click", () => {
+   card3.classList.toggle('is-flipped');
+
+ });
+ card4.addEventListener("click", () => {
+   card4.classList.toggle('is-flipped');
+
+ });
+ card5.addEventListener("click", () => {
+   card5.classList.toggle('is-flipped');
+
+ });
+ card6.addEventListener("click", () => {
+   card6.classList.toggle('is-flipped');
+
+ });
+
+ $('.navbar-nav>li>a').on('click', function(){
+   $('.navbar-collapse').collapse('hide');
+});
+window.addEventListener("scroll", () => {
+  const currentScroll = window.pageYOffset;
+  if (currentScroll <= 0) {
+    body.classList.remove(scrollUp);
+    return;
+  }
+  
+  if (currentScroll > lastScroll && !body.classList.contains(scrollDown)) {
+    // down
+    body.classList.remove(scrollUp);
+    body.classList.add(scrollDown);
+  } else if (currentScroll < lastScroll && body.classList.contains(scrollDown)) {
+    // up
+    body.classList.remove(scrollDown);
+    body.classList.add(scrollUp);
+  }
+  lastScroll = currentScroll;
+});
+
+
+jQuery(function ($) {
+
+   $(document).ready(function() {
+      var pageItem = $(".pagination li").not(".prev,.next");
+      var prev = $(".pagination li.prev");
+      var next = $(".pagination li.next");
+    
+      pageItem.click(function() {
+        pageItem.removeClass("active");
+        $(this).not(".prev,.next").addClass("active");
+      });
+    
+      next.click(function() {
+        $('li.active').removeClass('active').next().addClass('active');
+      });
+    
+      prev.click(function() {
+        $('li.active').removeClass('active').prev().addClass('active');
+      });
+    
+    
+    });
+
+   /**-------------------------------------------------
+    *Fixed HEader
+    *----------------------------------------------------**/
+   $(window).on('scroll', function () {
+
+      /**Fixed header**/
+      if ($(window).scrollTop() > 250) {
+         $('.header').addClass('sticky fade_down_effect');
+      } else {
+         $('.header').removeClass('sticky fade_down_effect');
+      }
+   });
+
+   /* ---------------------------------------------
+                     Menu Toggle 
+   ------------------------------------------------ */
+
+   if ($(window).width() < 991) {
+      $(".navbar-nav li a").on("click", function () {
+         $(this).parent("li").find(".dropdown-menu").slideToggle();
+         $(this).find("i").toggleClass("fa-angle-up fa-angle-down");
+      });
+
+   }
+
+
+   /* ----------------------------------------------------------- */
+   /*  Event counter 
+   /* -----------------------------------------------------------*/
+
+   if ($('.countdown').length > 0) {
+      $(".countdown").jCounter({
+         date: '17 January 2022 18:00:00',
+         fallback: function () {
+            console.log("count finished!")
+         }
+      });
+   }
+
+
+   /*==========================================================
+     funfact 
+     ======================================================================*/
+   var skl = true;
+   $('.ts-single-funfact').appear();
+
+   $('.ts-single-funfact').on('appear', function () {
+      if (skl) {
+         $('.counterUp').each(function () {
+            var $this = $(this);
+            jQuery({
+               Counter: 0
+            }).animate({
+               Counter: $this.attr('data-counter')
+            }, {
+               duration: 8000,
+               easing: 'swing',
+               step: function () {
+                  var num = Math.ceil(this.Counter).toString();
+                  if (Number(num) > 99999) {
+                     while (/(\d+)(\d{3})/.test(num)) {
+                        num = num.replace(/(\d+)(\d{3})/, '');
+                     }
+                  }
+                  $this.html(num);
+               }
+            });
+         });
+         skl = false;
+      }
+   });
+
+  
+
+   /*==========================================================
+          main slider
+  ======================================================================*/
+   if ($('.main-slider').length > 0) {
+      var bannerSlider = $(".main-slider");
+      bannerSlider.owlCarousel({
+         items: 1,
+         mouseDrag: true,
+         loop: true,
+         touchDrag: true,
+         autoplay:true,
+         dots: true,
+         autoplayTimeout: 50000,
+         animateOut: 'fadeOut',
+         autoplayHoverPause: true,
+         smartSpeed: 250,
+
+      });
+   }
+
+   /*=============================================================
+			 speaker popup
+	=========================================================================*/
+
+   $('.ts-image-popup').magnificPopup({
+      type: 'inline',
+      closeOnContentClick: false,
+      midClick: true,
+      callbacks: {
+         beforeOpen: function () {
+            this.st.mainClass = this.st.el.attr('data-effect');
+         }
+      },
+      zoom: {
+         enabled: true,
+         duration: 500, // don't foget to change the duration also in CSS
+      },
+      mainClass: 'mfp-fade',
+   });
+
+   /*=============================================================
+   			gallery
+   	=========================================================================*/
+
+   $('.ts-popup').magnificPopup({
+      type: 'image',
+      closeOnContentClick: false,
+      midClick: true,
+      callbacks: {
+         beforeOpen: function () {
+            this.st.mainClass = this.st.el.attr('data-effect');
+         }
+      },
+      zoom: {
+         enabled: true,
+         duration: 500, // don't foget to change the duration also in CSS
+      },
+      mainClass: 'mfp-fade',
+   });
+
+   /*=============================================================
+   			video popup
+   	=========================================================================*/
+
+   $('.ts-video-popup').magnificPopup({
+      type: 'iframe',
+      closeOnContentClick: false,
+      midClick: true,
+      callbacks: {
+         beforeOpen: function () {
+            this.st.mainClass = this.st.el.attr('data-effect');
+         }
+      },
+      zoom: {
+         enabled: true,
+         duration: 500, // don't foget to change the duration also in CSS
+      },
+      mainClass: 'mfp-fade',
+   });
+
+   /*=============================================================
+   			hero image animation
+   	=========================================================================*/
+   $('.tile')
+      // tile mouse actions
+      .on('mouseover', function () {
+         $(this).children('.photo').css({
+            'transform': 'scale(' + $(this).attr('data-scale') + ')'
+         });
+      })
+      .on('mouseout', function () {
+         $(this).children('.photo').css({
+            'transform': 'scale(1)'
+         });
+      })
+      .on('mousemove', function (e) {
+         $(this).children('.photo').css({
+            'transform-origin': ((e.pageX - $(this).offset().left) / $(this).width()) * 100 + '% ' + ((e.pageY - $(this).offset().top) / $(this).height()) * 100 + '%'
+         });
+      })
+      // tiles set up
+      .each(function () {
+         $(this)
+            // add a photo container
+            .append('<div class="photo"></div>')
+            // some text just to show zoom level on current item in this example
+            //.append('<div class="txt"><div class="x">'+ $(this).attr('data-scale') +'x</div>ZOOM ON<br>HOVER</div>')
+            // set up a background image for each tile based on data-image attribute
+            .children('.photo').css({
+               'background-image': 'url(' + $(this).attr('data-image') + ')'
+            });
+      });
+
+   /*==========================================================
+   wow animated
+    ======================================================================*/
+   var wow = new WOW({
+      animateClass: 'animated',
+      mobile: false
+   });
+   wow.init();
+
+
+   /* ----------------------------------------------------------- */
+   /*  Back to top
+   /* ----------------------------------------------------------- */
+
+   $(window).on('scroll', function () {
+      if ($(window).scrollTop() > $(window).height()) {
+         $(".BackTo").fadeIn('slow');
+      } else {
+         $(".BackTo").fadeOut('slow');
+      }
+
+   });
+   $("body, html").on("click", ".BackTo", function (e) {
+      e.preventDefault();
+      $('html, body').animate({
+         scrollTop: 0
+      }, 800);
+   });
+
+
+   
+   // scrollme.init();
+   if ($('.ts-scroll').length > 0) {
+      $(' .ts-scroll').on('click', function (e) {
+         e.preventDefault();
+         $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top - 85
+         }, 1000, 'linear');
+         return false;
+      });
+   }
+
+
+});
